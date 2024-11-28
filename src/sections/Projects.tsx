825b3da5fadafba3ff770fcf92bf0c589ba7eb58
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-11-28 15:46:49 +0600

    first part::hero home nav added

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,6 +3,9 @@ import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Link from "next/link";
 import Image from "next/image";
+import CheckBoxIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpButton from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
@@ -45,35 +48,82 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div>
+    <section className="pb-16 md:mt-12 ">
       <div className="container">
         <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r  from-emerald-300 to-sky-400 text-transparent bg-clip-text">Real-world Projects</p>
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r  from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+            Real-world Projects
+          </p>
         </div>
-        <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
-        <p>See how I transformed my clients need into digital solutions!</p>
-        <div>
+        <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4 max-w-md mx-auto md:text-lg">
+          See how I transformed my clients need into digital solutions!
+        </p>
+        <div className="flex flex-col mt-10 md:mt-20 gap-20">
           {portfolioProjects.map((project) => (
-            <div key={project.title}>
-              <div>
-                <span>{project.company}</span>
-                <span> {project.year}</span>
+            <div
+              key={project.title}
+              className="bg-gray-800 z-0 overflow-hidden rounded-3xl relative after:content[''] after:absolute after:inset-0 after:z-10 after:outline after:outline-2 after:-outline-offset-2 after:rounded-3xl 
+            after:outline-white/20 after:pointer-events-none px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20
+            "
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-12">
+                <div className="lg:pb-16">
+                  <div
+                    className="bg-gradient-to-r from-emerald-300 to-sky-400 
+                text-transparent bg-clip-text uppercase font-bold inline-flex tracking-widest text-sm gap-2
+                "
+                  >
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col mt-4  ">
+                    {project.results.map((result) => (
+                      <li
+                        key={result.title}
+                        className="flex gap-2 mt-4 text-white/50 text-sm md:text-base"
+                      >
+                        <CheckBoxIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <Link href={project.link}>
+                    <button className=" gap-2 justify-center items-center h-12 w-full bg-white text-gray-950 font-semibold rounded-xl mt-8 inline-flex md:w-auto md:px-4 ">
+                      <span className="font-bold">View Live Project</span>
+                      <ArrowUpButton className="size-4" />
+                    </button>
+                  </Link>
+                </div>
+
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0
+                    lg:absolute lg:h-full lg:w-auto lg:max-w-none
+                    "
+                  />
+                </div>
               </div>
-              <h3>{project.title}</h3>
-              <hr />
-              <ul>
-                {project.results.map((result) => (
-                  <li key={result.title}>{result.title}</li>
-                ))}
-              </ul>
-              <Link href={project.link}>
-                <button>View Live Project</button>
-              </Link>
-              <Image src={project.image} alt={project.title} />
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,37 @@
+import StarIcon from "@/assets/icons/star.svg";
+const words = [
+  "Performance",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Responsive",
+  "Scaleable",
+  "Fast",
+  "Modern",
+  "User Friendly",
+  "SEO Friendly",
+  "Cross Platform",
+  "Reliable",
+];
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex-none gap-2 py-3">
+            {words.map((word) => {
+              return (
+                <div key={word} className="inline-flex gap-2 items-center">
+                  <span className="text-gray-900 uppercase font-extrabold text-sm px-4">
+                    {word}
+                  </span>
+                  <StarIcon className="size-6 text-gray-900 -rotate-12 " />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
