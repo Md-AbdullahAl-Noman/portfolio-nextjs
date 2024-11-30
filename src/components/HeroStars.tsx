3164557e36c6e611a090ac3441b78bb954548609
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-11-30 18:34:05 +0600

    added the animation of orbit

--- src/components/HeroStars.tsx
+++ src/components/HeroStars.tsx
@@ -3,16 +3,38 @@ export const HeroStars = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number , rotation: number}>) => {
+  spinShouldOrbit = false,
+  orbitSpinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  orbitSpinDuration?: string;
+  spinShouldOrbit?: boolean;
+}>) => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className=""
-        style={{ width: `${size}px`, height: `${size}px` ,transform: `rotate(${rotation}deg)`}}
+        className={spinShouldOrbit && "animate-spin "}
+        style={{
+          animationDuration: orbitSpinDuration,
+        }}
       >
-        <div className="inline-flex "
-        style={{transform: `rotate(${rotation*-1}deg)`}}>
-          {children}
+        <div
+          className="flex items-start justify-start"
+          style={{
+            width: `${size}px`,
+            height: `${size}px`,
+            transform: `rotate(${rotation}deg)`,
+          }}
+        >
+          <div className="animate-spin [animation-duration:10s]">
+            <div
+              className="inline-flex "
+              style={{ transform: `rotate(${rotation * -1}deg)` }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,5 @@
 import ArrowUpIcon from "@/assets/icons/arrow-up-right.svg";
+import Link from "next/link";
 const footerLinks = [
   {
     title: "LinkedIn",
@@ -15,26 +16,23 @@ const footerLinks = [
 ];
 export const Footer = () => {
   return (
-    <footer className="relative mt-8 pb-96">
-      <div className="absolute h-[400px] w-[1600px] bottom-0 top-4 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]">
-        <div className="container">
-          <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
-            <div className="text-white/40">
-              &copy; 2024. All rights reserved.
-            </div>
-            <nav className="flex flex-col items-center gap-8 text-white">
-              {footerLinks.map((link) => (
-                <a
-                  key={link.title}
-                  href={link.url}
-                  className="inline-flex items-center gap-1.5"
-                >
-                  <span className="font-semibold">{link.title}</span>
-                  <ArrowUpIcon className="size-4" />
-                </a>
-              ))}
-            </nav>
-          </div>
+    <footer className="relative mt-8 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 ">
+          <div className="text-white/40">&copy; 2024. All rights reserved.</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8 text-white z-50">
+            {footerLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.url}
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpIcon className="size-4" />
+              </Link>
+            ))}
+          </nav>
         </div>
       </div>
     </footer>
