00489017151d37544c34347b710ecbf59f17989c
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-12-26 13:22:44 +0600

    fixed the nav style

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,21 +1,55 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export const Header = () => {
+  const [selected, setSelected] = useState("home");
+  const handleNavClick = (section: string) => {
+    setSelected(section);
+  };
   return (
     <div className="flex justify-center items-center pt-3  w-[100vw] fixed [z-index:100] backdrop:blur-xl">
       <nav className="flex p-0.5 gap-1 border border-white/15 rounded-full bg-white/10">
-        <a href="#home" className="nav-item">
+        <a
+          href="#home"
+          className={`nav-item ${
+            selected === "home"
+              ? "bg-white text-gray-900 animate-pulse duration-1000"
+              : ""
+          }`}
+          onClick={() => handleNavClick("home")}
+        >
           Home
         </a>
-        <a href="#projects" className="nav-item">
+        <a
+          href="#projects"
+          className={`nav-item ${
+            selected === "projects"
+              ? "bg-white text-gray-900 animate-pulse duration-1000"
+              : ""
+          }`}
+          onClick={() => handleNavClick("projects")}
+        >
           Projects
         </a>
-        <a href="#about" className="nav-item">
+        <a
+          href="#about"
+          className={`nav-item ${
+            selected === "about"
+              ? "bg-white text-gray-900 animate-pulse duration-1000"
+              : ""
+          }`}
+          onClick={() => handleNavClick("about")}
+        >
           About
         </a>
         <a
           href="#contact"
-          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+          className={`nav-item ${
+            selected === "contact"
+              ? "bg-white text-gray-900 animate-pulse duration-1000"
+              : ""
+          }`}
+          onClick={() => handleNavClick("contact")}
         >
           Contact
         </a>
